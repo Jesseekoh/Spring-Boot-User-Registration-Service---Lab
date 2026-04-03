e27290d34dbbfbaf983daecb226a09cf236d529a
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2026-04-03 20:41:27 +0100

    feat: initialize user registration service project with base structure

---
+++ src/test/java/com/jesseekoh/user_registration_service/UserRegistrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jesseekoh.user_registration_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegistrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
